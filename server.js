24bb948e10b923626e7e98aa1dedaadb30e5041d
sunjo <sunjo@prismweaver.com>
2022-07-14 12:05:52 +0900

    4.11 Migrating from REST to GraphQL

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import { ApolloServer, gql } from "apollo-server";
+import fetch from "node-fetch";
 
 let tweets = [
     {
         id: "1",
         text: "first one!",
+        userId: "2",
     },
     {
         id: "2",
         text: "second one",
+        userId: "1",
     },
 ];
 
@@ -29,21 +32,55 @@ const typeDefs = gql`
     id: ID!
     firstName: String!
     lastName: String!
+    """
+    Is the sum of firstName + lastName as a string
+    """
     fullName: String!
   }
+  """
+  Tweet object represents a resource for  a Tweet
+  """
   type Tweet {
     id: ID!
     text: String!
     author: User
   }
   type Query {
+    allMovies: [Movie!]!
     allUsers: [User!]!
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
+    movie(id: String!): Movie
   }
   type Mutation {
     postTweet(text: String!, userId: ID!): Tweet!
+    """
+    Deletes a Tweet if found, else returns false
+    """
     deleteTweet(id: ID!): Boolean!
+  }
+  type Movie {
+    id: Int!
+    url: String!
+    imdb_code: String!
+    title: String!
+    title_english: String!
+    title_long: String!
+    slug: String!
+    year: Int!
+    rating: Float!
+    runtime: Float!
+    genres: [String]!
+    summary: String
+    description_full: String!
+    synopsis: String
+    yt_trailer_code: String!
+    language: String!
+    background_image: String!
+    background_image_original: String!
+    small_cover_image: String!
+    medium_cover_image: String!
+    large_cover_image: String!
   }
 `;
 // GET /api/v1/tweets
@@ -62,12 +99,23 @@ const resolvers = {
             console.log("allUsers called!");
             return users;
         },
+        allMovies() {
+            return fetch("https://yts.mx/api/v2/list_movies.json")
+                .then((r) => r.json())
+                .then((json) => json.data.movies);
+        },
+        movie(_, { id }) {
+            return fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+                .then((r) => r.json())
+                .then((json) => json.data.movie);
+        },
     },
     Mutation: {
         postTweet(_, { text, userId }) {
             const newTweet = {
                 id: tweets.length + 1,
                 text,
+                userId,
             };
             tweets.push(newTweet);
             return newTweet;
@@ -82,6 +130,11 @@ const resolvers = {
     User: {
         fullName({ firstName, lastName }) {
             return `${firstName} ${lastName}`;
+        },
+    },
+    Tweet: {
+        author({ userId }) {
+            return users.find((user) => user.id === userId);
         },
     },
 };
